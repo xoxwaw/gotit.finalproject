2ee4d330323bafdc7bb182a7042c81c180ab01ac
hungphi98 <nguyenhungphi2698@Gmail.com>
2020-05-27 12:28:57 +0700

    fix: fix debugging messages

--- main/config/__init__.py
+++ main/config/__init__.py
@@ -1,16 +1,24 @@
 import logging
 import os
+import sys
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
 
 def validate_env(name):
-    var = None
-    try:
-        var = os.getenv(name)
-    except EnvironmentError:
+    var = os.getenv(name)
+    if var is None:
         logging.error('{} must be specified in .env file'.format(name))
-        exit(0)
+        exit(1)
     return var
